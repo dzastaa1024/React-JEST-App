d5456a4190b7fe8d7cbc51bcdd26507d22766212
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-06-01 13:42:42 +0100

    verifying component existence

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CommentBox from "./CommetBox";
+import CommentList from "./CommentList";
+
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
