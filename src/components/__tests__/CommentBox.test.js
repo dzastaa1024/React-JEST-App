e8c350cb8f6174be0dc5f36c3035ba3e3db04956
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-06-04 10:58:47 +0100

    testing submit

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -25,3 +25,15 @@ it("has a text area that users can type in", () => {
 
   expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 });
+
+it("when form is submitted, text area gets emtied", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+  wrapped.update();
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  wrapped.find("textarea").prop();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
+});
