2b0fd800a04cc388f41b9796e3c36782f33a1a1e
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-06-06 10:37:10 +0100

    .

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "reducers";
+import reduxPromise from "redux-promise";
 
-export default (props) => {
-  return (
-    <Provider store={createStore(reducers, {})}>{props.children}</Provider>
+export default ({ children, initialState }) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise)
   );
+  return <Provider store={store}>{children}</Provider>;
 };
