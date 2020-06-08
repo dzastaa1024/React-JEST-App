03fb5bfb8c3fa469e9d69f9fc4acb5e1d888b768
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-06-08 11:59:17 +0100

    router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import Root from "./Root";
 import { createStore } from "redux";
 import reducers from "./reducers";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import App from "./components/App";
 
 ReactDOM.render(
   <Root store={createStore(reducers, {})}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>,
   document.querySelector("#root")
 );
